17f5d58fa4e58ea9b333df207c849fab0416edcc
arnlaugs <arnlaugskj@gmail.com>
2018-10-16 16:49:31 +0200

    Bygget litt videre

--- codefrompaper.py
+++ codefrompaper.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy.sparse as sp
-np.random.seed(12)
+import sys
+sys.path.append('../Fys-Stk-Project-1/functions/')
+from regression import OLS
+import matplotlib.pyplot as plt
+np.random.seed(14)
 
 import warnings
 #Comment this to turn on warnings
@@ -11,7 +15,7 @@ warnings.filterwarnings('ignore')
 L=40
 
 # create 10000 random Ising states
-states=np.random.choice([-1, 1], size=(10000,L))
+states=np.random.choice([-1, 1], size=(400,L))
 
 def ising_energies(states,L):
     """
@@ -27,4 +31,27 @@ def ising_energies(states,L):
 # calculate Ising energies
 energies=ising_energies(states,L)
 
-print(energies)
+#print(energies)
+states=np.einsum('...i,...j->...ij', states, states)
+#print(states)
+shape=states.shape
+states=states.reshape((shape[0],shape[1]*shape[2]))
+# build final data set
+Data=[states,energies]
+coefs_OLS=[]
+x=Data[0]
+y=Data[1]
+
+model=OLS()
+
+coefs_OLS.append(model.fit(x,y,ret=True))
+
+coef_OLS_plot=np.array(coefs_OLS).reshape(40,40)
+for i in range(L):
+    print(coef_OLS_plot[i,(i+1)%L])
+cmap_args=dict(vmin=-1., vmax=1., cmap='seismic')
+plt.imshow(coef_OLS_plot,**cmap_args)
+plt.colorbar()
+
+plt.show()
+
